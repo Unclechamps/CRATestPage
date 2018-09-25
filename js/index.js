e558b2037c40677910105c11a9523b3fac76ab42
Gabe <36457852+Unclechamps@users.noreply.github.com>
2018-09-25 13:11:53 -0500

    cambios

--- js/index.js
+++ js/index.js
@@ -1,7 +1,26 @@
-let blogPost = document.getElementById('blog')
+let blogPost = document.querySelector('.blog')
 
 const API_KEY = "AIzaSyBSsuC2lj-sPe5DzhsTVrTX24_RgXsqdOQ"
+const BLOG_ID = "330206725513860697"
+const API_URL = "https://www.googleapis.com/blogger/v3/blogs/"+BLOG_ID+"/posts?key=" + API_KEY
 
-function populateBlogs(listOfBlogs) {
-
-}
+fetch(API_URL)
+  .then(response => {
+    return response.json()
+  }).then(posts => {
+    console.log(posts.items)
+    posts.items.forEach(post => {
+      let blogPostDetail = `<div className="post">
+            <div className='top'>
+              <h2>${post.title}</h2>
+              <h5>by: ${post.author.displayName}</h5>
+            </div>
+            <div className="content">
+              <p>${post.content}</p>
+            </div>
+            <hr />
+            </div>
+            `
+      blogPost.innerHTML += blogPostDetail
+    })
+  })
